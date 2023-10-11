bc64e3f7a0ff4db5542b705598d024fda345cbe8
Ira <sokurira@gmail.com>
2023-10-11 23:40:40 +0300

    task 4

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import random
-while True:
-    try:
-        NUM_SIZE = int(input("Enter size of list: "))
-        if NUM_SIZE > 0:
-            break
-        if NUM_SIZE <= 0:
-            print("You should to enter number more then 0")
-    except ValueError:
-        print("Enter only integer numbers please!")
-
-numbers = []
-for num in range(NUM_SIZE):
-    numbers.append(random.randint(1, 100))
-
-print(numbers)
+# import random
+# while True:
+#     try:
+#         NUM_SIZE = int(input("Enter size of list: "))
+#         if NUM_SIZE > 0:
+#             break
+#         if NUM_SIZE <= 0:
+#             print("You should to enter number more then 0")
+#     except ValueError:
+#         print("Enter only integer numbers please!")
+#
+# numbers = []
+# for num in range(NUM_SIZE):
+#     numbers.append(random.randint(1, 100))
+#
+# print(numbers)
 
 #1
 # def multiplication_list(num) -> int:
@@ -31,20 +31,39 @@ print(numbers)
 # print(f"Minimum in the list: {find_min(numbers)}")
 
 #3
-def counter_of_prime_numbers(num):
-    counter = 0
-    for number in num:
-        if number < 2:
-            continue
+# def counter_of_prime_numbers(num):
+#     counter = 0
+#     for number in num:
+#         if number < 2:
+#             continue
+#
+#         is_simple = True
+#
+#         for i in range(2, number):
+#              if number % i == 0:
+#                 is_simple = False
+#                 break
+#         if is_simple:
+#             counter += 1
+#
+#     return counter
+# print(f"The number of primes in a list: {counter_of_prime_numbers(numbers)}")
 
-        is_simple = True
+#4
+list_for_del = [1, 2, 3, 4, 5, 6, 7, 1, 4]
+def del_element_in_list(num):
+    counter_del = 0
+    for_del = 4
+    for x in num:
+        if x == for_del:
+            counter_del += 1
 
-        for i in range(2, number):
-             if number % i == 0:
-                is_simple = False
-                break
-        if is_simple:
-            counter += 1
+    if counter_del != 0:
+        num.remove(for_del)
 
-    return counter
-print(f"The number of primes in a list: {counter_of_prime_numbers(numbers)}")
+    return counter_del
+
+if del_element_in_list(list_for_del) != 0:
+    print(f"Counter of deleted elements: {del_element_in_list(list_for_del)}")
+else:
+    print("Nothing found")
