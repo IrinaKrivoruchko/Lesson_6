64dc0d04a8efcc1810ae20d8d267e8cb83d6796c
Ira <sokurira@gmail.com>
2023-10-11 22:42:59 +0300

    counter of prime numbers

--- main.py
+++ main.py
@@ -11,21 +11,40 @@ while True:
 
 numbers = []
 for num in range(NUM_SIZE):
-    numbers.append(random.randint(-100, 100))
+    numbers.append(random.randint(1, 100))
 
 print(numbers)
 
 #1
-def multiplication_list(num) -> int:
-    res_multi = 1
-    for i in num:
-        res_multi *= i
-    return res_multi
-
-print(f"Result of multiplication: {multiplication_list(numbers)}")
+# def multiplication_list(num) -> int:
+#     res_multi = 1
+#     for i in num:
+#         res_multi *= i
+#     return res_multi
+#
+# print(f"Result of multiplication: {multiplication_list(numbers)}")
 
 #2
-def find_min(num_min) -> int:
-    return min(num_min)
+# def find_min(num_min) -> int:
+#     return min(num_min)
+#
+# print(f"Minimum in the list: {find_min(numbers)}")
 
-print(f"Minimum in the list: {find_min(numbers)}")
+#3
+def counter_of_prime_numbers(num):
+    counter = 0
+    for number in num:
+        if number < 2:
+            continue
+
+        is_simple = True
+
+        for i in range(2, number):
+             if number % i == 0:
+                is_simple = False
+                break
+        if is_simple:
+            counter += 1
+
+    return counter
+print(f"The number of primes in a list: {counter_of_prime_numbers(numbers)}")
